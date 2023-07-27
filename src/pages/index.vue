e6239880995aa7c9aef3c5c968bd46afed828bb9
Akayy <techakayy@gmail.com>
2023-07-27 15:06:28 +1000

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<page>
+    title: Nature's Delight!
+</page>
+<template>
+  <main>
+    <HeroSection client:load />
+    <!-- <StatsSection client:visible /> -->
+    <StatsSection />
+    <FeatureSection client:visible />
+    <!-- <TestimonialSection client:visible /> -->
+    <TestimonialSection />
+    <SubscribeSection client:visible />
+  </main>
+</template>
